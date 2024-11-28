12f70379a1e039ea1ebe75fe4176319961220e3e
Kayan Laswell <kcclaswell@gmail.com>
2024-11-27 20:34:45 -0500

    Add switch function

--- src/converter.c
+++ src/converter.c
@@ -1,6 +1,71 @@
 #include <stdio.h>
 
-int main(){
 
-    printf("")
+int hexToInt(char hex){
+    int num;
+
+    switch(hex){
+    case '1':
+        num = 1;
+        break;
+    case '2':
+        num = 2;
+        break;
+    case '3':
+        num = 3;
+        break;
+    case '4':
+        num = 4;
+        break;
+    case '5':
+        num = 5;
+        break;
+    case '6':
+        num = 6;
+        break;
+    case '7':
+        num = 7;
+        break;
+    case '8':
+        num = 8;
+        break;
+    case '9':
+        num = 9;
+        break;
+    case 'A':
+        num = 10;
+        break;
+    case 'B':
+        num = 11;
+        break;
+    case 'C':
+        num = 12;
+        break;
+    case 'D':
+        num = 13;
+        break;
+    case 'E':
+        num = 14;
+        break;
+    case 'F':
+        num = 15;
+        break;
+    default:
+        printf("Wrong input\n");
+        break;
+    }
+
+    return num;
+}
+int main(){
+    char hex;
+
+    while (hex != 'x'){
+        printf("Enter a number in hexadecimal between 0-E: \n");
+        scanf("%c", &hex);
+        int converted = hexToInt(hex);
+        printf("Converted integer: %d\n", converted);
+    }
+
+    return 0;
 }
